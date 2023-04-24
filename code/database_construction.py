db69c3445a51ec9c7e294d063c970dace07246c7
aymanetabbai <tabbai.aymane@gmail.com>
2023-04-24 11:12:20 +0200

    add sector database

--- code/database_construction.py
+++ code/database_construction.py
@@ -3,4 +3,5 @@ import openpyxl
 
 we_green=pd.read_excel("../data/data_wegreen.xlsx")
 chatgpt=pd.read_excel("../data/Chatgpt_data.xlsx")
-data = pd.merge(we_green, chatgpt, left_on=[we_green.columns[0],we_green.columns[1]], right_on=[chatgpt.columns[0],chatgpt.columns[1]], how='outer')
+sector=pd.read_excel("../data/wikipedia_secteur.xlsx")
+data_carbon = pd.merge(we_green, chatgpt, left_on=[we_green.columns[0],we_green.columns[1]], right_on=[chatgpt.columns[0],chatgpt.columns[1]], how='outer')
